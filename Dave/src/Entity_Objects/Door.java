f161d7bd39b051fc0e240b9a0010a71441fd9c34
halilatmaca <selmanatmaca@yahoo.com.tr>
2017-07-30 19:07:17 +0300

    Add files via upload

---
+++ Dave/src/Entity_Objects/Door.java
@@ -0,0 +1,10 @@
+package Entity_Objects;
+
+public class Door extends MapObject{
+
+	public Door(double X, double Y, String I) {
+		super(X, Y,"/door.png");
+		// TODO Auto-generated constructor stub
+	}
+
+}
