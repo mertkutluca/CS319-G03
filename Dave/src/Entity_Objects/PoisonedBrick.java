3b79b6ed318f26e1ebe5fe53fc2e0bba046e1ffd
halilatmaca <selmanatmaca@yahoo.com.tr>
2017-07-30 19:06:18 +0300

    Add files via upload

---
+++ Dave/src/Entity_Objects/PoisonedBrick.java
@@ -0,0 +1,10 @@
+package Entity_Objects;
+
+public class PoisonedBrick extends MapObject{
+
+	public PoisonedBrick(double X, double Y, String I) {
+		super(X, Y,"/poisonedBrick.png");
+		// TODO Auto-generated constructor stub
+	}
+
+}
