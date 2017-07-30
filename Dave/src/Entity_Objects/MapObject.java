b4ab968637cee7a7f90778514b1f7658279b0c92
halilatmaca <selmanatmaca@yahoo.com.tr>
2017-07-30 19:05:52 +0300

    Add files via upload

---
+++ Dave/src/Entity_Objects/MapObject.java
@@ -0,0 +1,10 @@
+package Entity_Objects;
+
+public class MapObject extends StaticGameObject {
+
+	public MapObject(double X, double Y, String I) {
+		super(X, Y, I);
+		// TODO Auto-generated constructor stub
+	}
+
+}
