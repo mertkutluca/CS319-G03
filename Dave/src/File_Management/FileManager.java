102655a21f1e4324a195c452732435ef5524f7aa
halilatmaca <selmanatmaca@yahoo.com.tr>
2017-07-31 04:41:24 +0300

    Update FileManager.java

--- Dave/src/File_Management/FileManager.java
+++ Dave/src/File_Management/FileManager.java
@@ -17,6 +17,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Collections;
 
 public class FileManager {
@@ -45,6 +46,74 @@ public void readMapFile(String filePath) {
 		//GameApplication.alertIOException(filePath);
 		System.out.println("IOException");
 	}
+}
+public void readHighScore(String filepath){
+    FileReader fr;
+    //Try-with-resource  
+	try(BufferedReader br = new BufferedReader(new FileReader(filepath))){
+		 
+                for(int i=0;i<10;i++){
+                    highScores.add(Integer.parseInt(br.readLine()));
+                }
+	}catch(FileNotFoundException e){
+		//GameApplication.alertFileNotFound(filePath);
+		System.out.println("FileNotFound");
+	}catch(IOException e){
+		//GameApplication.alertIOException(filePath);
+		System.out.println("IOException");
+	}
+}
+public void addHighScore(int score){
+    highScores.add(score);
+    Collections.sort(highScores);
+    highScores.remove(0);
+}
+public void writeHighScores(String filepath) throws FileNotFoundException{
+		BufferedWriter bw = null;
+		FileWriter fw = null;
+		PrintWriter writer = new PrintWriter(filepath);          
+                writer.print("");
+                writer.close();
+		try {
+			fw = new FileWriter(filepath);
+			bw = new BufferedWriter(fw);
+                        
+			for(int i=0;i<10;i++){
+				bw.write(String.valueOf(highScores.get(i))+"\n");
+			}
+
+			System.out.println("Done");
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			try {
+
+				if (bw != null)
+					bw.close();
+
+				if (fw != null)
+					fw.close();
+
+			} catch (IOException ex) {
+
+				ex.printStackTrace();
+
+			}
+
+		}
+	
+}
+public void addScore(String filepath,int score) throws FileNotFoundException{
+    readHighScore(filepath);
+    addHighScore(score);
+    writeHighScores(filepath);
+}
+public ArrayList<Integer> getHighScores(){
+    return highScores;
 }
 public char[][] getMapObjects(){
 	return mapObjectList;
